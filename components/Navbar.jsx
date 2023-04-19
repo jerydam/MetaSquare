4dc87b554a75ba87dfe729f93b63b4606f6a2b8a
Jerydam <jerydan148@gmail.com>
2023-04-19 04:04:37 +0100

    what going on

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -16,11 +16,6 @@ const Navbar = () => (
     <div
       className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}
     >
-      <img
-        src="/search.svg"
-        alt="search"
-        className="w-[24px] h-[24px] object-contain"
-      />
        <a href='/'className="font-extrabold text-[24px] leading-[30.24px] text-white">
         METASQUARE
       </a>
--- pages/_app.js
+++ pages/_app.js
@@ -37,7 +37,7 @@ const MyApp = ({ Component, pageProps }) => (
     <Head>
       <title>MetaSquare</title>
       <meta name="viewport" content="width=device-width, initial-scale=1" />
-      <link rel="icon" href="/favicon.ico" />
+      <link rel="icon" href="/meta.png" />
       <link rel="preconnect" href="https://stijndv.com" />
       <link rel="stylesheet" href="https://stijndv.com/fonts/Eudoxus-Sans.css" />
     </Head>
