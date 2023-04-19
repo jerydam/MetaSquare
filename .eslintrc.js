6cc229afc098d226b9d220c86fa8cbc3888a9ade
Jerydam <jerydan148@gmail.com>
2023-04-19 11:43:38 +0100

    iro

--- .eslintrc.js
+++ .eslintrc.js
@@ -18,7 +18,7 @@ module.exports = {
     'react',
   ],
   rules: {
-    'react/no-unescaped-entities': false,
+    'react/no-unescaped-entities': 0,
     'eslintreact/no-danger': 0,
     'react/jsx-max-props-per-line': 0,
     'react/jsx-first-prop-new-line': 0,
