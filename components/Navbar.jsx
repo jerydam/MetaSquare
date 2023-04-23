03563d1405af8a410eaee486b29b9016d61c2dda
Jerydam <jerydan148@gmail.com>
2023-04-23 14:00:49 +0100

    Navbar update

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -7,39 +7,58 @@ import { navVariants } from "../utils/motion";
 import Connect from "./Connect";
 
 const Navbar = () => (
+    
   <motion.nav
     variants={navVariants}
     initial="hidden"
     whileInView="show"
     className={`${styles.xPaddings} py-8 relative`}
   >
-    <div className="absolute w-[50%] inset-0 gradient-01" />
-    <div className={`${styles.innerWidth} mx-auto flex justify-between gap-8`}>
-      <a
-        href="/"
-        className="font-extrabold text-[24px] leading-[30.24px] text-white"
-      >
-        METASQUARE
-      </a>
-      <a
-        href=""
-        className="font-extrabold text-[24px] leading-[30.24px] text-white"
-      >
-        <a href="https://dads-organization.gitbook.io/untitled/">
-          <button>Events</button>
+      <motion.nav
+  variants={navVariants}
+  initial="hidden"
+  whileInView="show"
+  className={`${styles.xPaddings} py-8 relative`}
+>
+  <div className="flex flex-wrap items-center justify-between">
+    <div className="flex items-center flex-shrink-0 text-white mr-6">
+        <a href="/">
+          <span className="font-bold text-xl text-[#fff] tracking-tight ml-2">
+            <span className="text-5xl text-red">.</span>METASQUARE
+          </span>
         </a>
-      </a>
-       <a
-        href=""
-        className="font-extrabold text-[24px] leading-[30.24px] text-white"
-      >
-        <a href="/events">
-          <button>Docs</button>
-        </a>
-      </a>
-
-      <Connect />
     </div>
+
+    <div className="w-full block flex-grow ml-[300px] lg:flex lg:items-center lg:w-auto">
+      <div className="text-md lg:flex-grow">
+        <a
+          href="/events"
+          className="block mt-4 lg:inline-block lg:mt-0 text-gray-500 font-bold hover:text-white mr-10"
+        >
+          Events
+        </a>
+        <a
+          href="#responsive-header"
+          className="block mt-4 lg:inline-block mr-10 font-bold lg:mt-0 text-gray-500 hover:text-white"
+        >
+          Devs
+        </a>
+        <a
+          href="https://dads-organization.gitbook.io/untitled/"
+          className="block mt-4 lg:inline-block mr-10 font-bold lg:mt-0 text-gray-500 hover:text-white"
+        >
+          Docs
+        </a>
+       
+      </div>
+
+      <div className="ml-auto">
+        <Connect />
+      </div>
+    </div>
+  </div>
+</motion.nav>
+
   </motion.nav>
 );
 export default Navbar;
